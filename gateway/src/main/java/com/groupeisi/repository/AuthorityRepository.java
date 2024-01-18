db4bda81cb4cfc854e2fe205a800a7ccf11369ff
Tidiany <tidianycorporation337@gmail.com>
2024-01-18 19:27:25 +0000

    Initial version of gateway generated by generator-jhipster@8.1.0

---
+++ gateway/src/main/java/com/groupeisi/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.groupeisi.repository;
+
+import com.groupeisi.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
